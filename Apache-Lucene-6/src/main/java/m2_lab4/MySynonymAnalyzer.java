5cfa1f5aabbfdb67b9253c593438b541cfeeecbe
Asif Iqbal <asif.31.iqbal@gmail.com>
2017-02-01 23:29:59 -0500

    More trials

--- Apache-Lucene-6/src/main/java/m2_lab4/MySynonymAnalyzer.java
+++ Apache-Lucene-6/src/main/java/m2_lab4/MySynonymAnalyzer.java
@@ -8,7 +8,8 @@ import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.core.LowerCaseFilter;
 import org.apache.lucene.analysis.standard.ClassicTokenizer;
 import org.apache.lucene.analysis.standard.StandardFilter;
-import org.apache.lucene.analysis.synonym.SynonymFilter;
+import org.apache.lucene.analysis.synonym.FlattenGraphFilter;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilter;
 import org.apache.lucene.analysis.synonym.SynonymMap;
 import org.apache.lucene.util.CharsRef;
 
@@ -24,7 +25,8 @@ public class MySynonymAnalyzer extends Analyzer {
 		Tokenizer source = new ClassicTokenizer();
 		TokenStream filter = new StandardFilter(source);
 		filter = new LowerCaseFilter(filter);
-		filter = new SynonymFilter(filter, getSynonymsMap(), false);
+		filter = new SynonymGraphFilter(filter, getSynonymsMap(), false);
+		filter = new FlattenGraphFilter(filter);
 		return new TokenStreamComponents(source, filter);
 	}
 
@@ -32,13 +34,13 @@ public class MySynonymAnalyzer extends Analyzer {
 	private SynonymMap getSynonymsMap() {
 		try {
 			SynonymMap.Builder builder = new SynonymMap.Builder(true);
-//			builder.add(new CharsRef("finished"), new CharsRef("completed"),
-//					true);
-//			builder.add(new CharsRef("finished"), new CharsRef("over"), true);
-//			builder.add(new CharsRef("finished"), new CharsRef("ended"), true);
-//			builder.add(new CharsRef("work"), new CharsRef("job"), true);
-//			builder.add(new CharsRef("work"), new CharsRef("labor"), true);
-//			builder.add(new CharsRef("work"), new CharsRef("effort"), true);
+			builder.add(new CharsRef("finished"), new CharsRef("completed"),
+					true);
+			builder.add(new CharsRef("finished"), new CharsRef("over"), true);
+			builder.add(new CharsRef("finished"), new CharsRef("ended"), true);
+			builder.add(new CharsRef("work"), new CharsRef("job"), true);
+			builder.add(new CharsRef("work"), new CharsRef("labor"), true);
+			builder.add(new CharsRef("work"), new CharsRef("effort"), true);
 			SynonymMap mySynonymMap = builder.build();
 			return mySynonymMap;
 		} catch (Exception ex) {
